02c282d2a7e0ee0c8b1997e738fa7baf1ca7b80d
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-09-16 09:52:48 +0300

    Add Google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,15 @@ module.exports = {
         ]
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-110067126-1",
+        head: false,
+        anonymize: true,
+        respectDNT: true
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-catch-links',
     'gatsby-plugin-netlify'
